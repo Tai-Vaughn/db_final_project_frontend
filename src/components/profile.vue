1ba061bb494c6d4f78c3d6bc344c3a872d765120
Tai-Vaughn White <Isamutai200@gmail.com>
2020-05-02 22:28:46 -0500

    syntax changes

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,49 +1,49 @@
 
-        <template>
-            <v-container fluid>
-                <v-layout column>
+<template>
+    <v-container fluid>
+        <v-layout column>
 
-                  <v-card-title class="card-header text-secondary">
-                        @{{ user.username }}
-                  </v-card-title>
+            <v-card-title class="card-header text-secondary">
+                @{{ user.username }}
+            </v-card-title>
 
 
-                    <v-card>
-                        <v-card-text>
+            <v-card>
+                <v-card-text>
 
-                            <v-flex class="mb-4">
+                    <v-flex class="mb-4">
 
-                                <v-avatar size="96" class="mr-4">
-                                    <img :src="'/avatars/avatar_' +  {{ user.profile_pic }} + '.png'" alt="Avatar">
-                                </v-avatar>
+                        <v-avatar size="96" class="mr-4">
+                            <img :src="'/avatars/avatar_' +  {{ user.profile_pic }} + '.png'" alt="Avatar">
+                        </v-avatar>
 
-                                <v-btn>Change Avatar</v-btn>
+                        <v-btn>Change Avatar</v-btn>
 
-                            </v-flex>
+                    </v-flex>
 
-                            <h5>Name: {{ user.first_name }} {{user.last_name}}</h5>
+                    <h5>Name: {{ user.first_name }} {{user.last_name}}</h5>
 
-                            <h5>DOB:  {{user.DOB}}</h5>
+                    <h5>DOB:  {{user.DOB}}</h5>
 
-                            <h5>Email: {{user.email }}</h5>
+                    <h5>Email: {{user.email }}</h5>
 
-                        </v-card-text>
+                </v-card-text>
 
-                        <v-card-actions>
-                           <v-icon right fab color="cyan">mdi-pencil</v-icon>
-                                Make Changes 
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
+                <v-card-actions>
+                    <v-icon right fab color="cyan">mdi-pencil</v-icon>
+                        Make Changes 
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
 
-                </v-layout>
-             </v-container>
-        </template>
+        </v-layout>
+        </v-container>
+</template>
 
-        <script>
-            export default {
-                pageTitle: 'My Profile',
-                name:"Profile"
-                props:['user']
-                }
-        </script>
+<script>
+    export default {
+        pageTitle: 'My Profile',
+        name:"Profile"
+        props:['user']
+        }
+</script>
